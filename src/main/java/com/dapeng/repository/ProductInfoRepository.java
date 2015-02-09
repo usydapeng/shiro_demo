e7504d990d9862b3977bc713a5928dd34ff69a0e
usydapeng <usydapeng@163.com>
2015-02-09 14:39:08 +0800

    修改表约束的 注解方式

--- src/main/java/com/dapeng/repository/ProductInfoRepository.java
+++ src/main/java/com/dapeng/repository/ProductInfoRepository.java
@@ -1,19 +1,21 @@
 package com.dapeng.repository;
 
 import com.dapeng.domain.ProductInfo;
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@CacheConfig(cacheNames = "product")
 public interface ProductInfoRepository extends JpaRepository<ProductInfo, Long> {
 
 	@Override
-	@CacheEvict("product")
+	@CacheEvict(allEntries = true)
 	<S extends ProductInfo> S save(S entity);
 
 	@Override
-	@Cacheable("product")
+	@Cacheable
 	List<ProductInfo> findAll();
 }
